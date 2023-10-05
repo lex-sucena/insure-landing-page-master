ae51ee8ec55055cab24d5e96f404b7326d04934c
lex-sucena <alex.everythingelse@gmail.com>
2023-10-05 11:29:35 -0300

    Added functionality to the nav button

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const mobileMenuBtn = document.getElementById("mobileNavBtn");
 const headerMenu = document.getElementById("menu");
+const mobileBtnImg = document.getElementById("btnImg");
 function toggleMenu() {
-  headerMenu.classList.toggle(".block");
+  headerMenu.classList.toggle("block");
+  mobileBtnImg.classList.toggle("changeImgSrc");
 }
 mobileMenuBtn.addEventListener("click", toggleMenu);
